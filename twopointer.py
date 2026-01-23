a257cd3cf489c5e99ad606dd59f15e02393c9db0
zarifkhan-07 <zarifsuhana1@gmail.com>
2026-01-23 18:53:34 +0600

    Add files via upload

--- twopointer.py
+++ twopointer.py
@@ -1,21 +1,14 @@
-def isPairSum(A, N, X):
-
-    for i in range(N):
-        for j in range(N):
-
-            if(i == j):
-                continue
-
-            if (A[i] + A[j] == X):
-                return [A[i], A[j]]
-
-            if (A[i] + A[j] > X):
-                break
-
+def ispairsum(A,N,X):
+    i=0
+    j=N-1
+    while(i<j):
+        if(A[i]+A[j]==X):
+            return [A[i],A[j]]
+        elif(A[i]+A[j]<X):
+            i+=1
+        else:
+            j-=1
     return 0
-
-arr = [2, 3, 5, 8, 9, 10, 11]
-
-val = 17
-
-print("Pair with the sum equal to {} is - {}".format(val, isPairSum(arr, len(arr), val)))
+arr=[2,3,5,8,9,10,11]
+val=17
+print("Pair wih the sum equal to {} is {}".format(val, ispairsum(arr,len(arr), val)))
