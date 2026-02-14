5a6287c701085cc1159b80669da91f1be0c1d47d
zarifkhan-07 <zarifsuhana1@gmail.com>
2026-02-14 18:58:38 +0600

    Add files via upload

---
+++ merge2sorted arr.py
@@ -0,0 +1,31 @@
+def merge_sorted_arrays(arr1, arr2):
+    i = 0
+    j = 0
+    merged = []
+
+    # Compare elements from both arrays
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] <= arr2[j]:
+            merged.append(arr1[i])
+            i += 1
+        else:
+            merged.append(arr2[j])
+            j += 1
+
+    # Add remaining elements (if any)
+    while i < len(arr1):
+        merged.append(arr1[i])
+        i += 1
+
+    while j < len(arr2):
+        merged.append(arr2[j])
+        j += 1
+
+    return merged
+
+
+# Example usage
+arr1 = [1, 3, 5, 7]
+arr2 = [2, 4, 6, 8]
+result = merge_sorted_arrays(arr1, arr2)
+print("Merged array:", result)
